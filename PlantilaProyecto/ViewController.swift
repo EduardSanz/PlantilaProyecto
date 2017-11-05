b12770c744bbe2718c475abd030628a2d1b24b9d
Sergio Becerril <cfeapps@gmail.com>
2017-11-05 13:27:35 +0100

    Initial Commit

---
+++ PlantilaProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlantilaProyecto
+//
+//  Created by Sergio Becerril on 5/11/17.
+//  Copyright © 2017 Sergio Becerril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
